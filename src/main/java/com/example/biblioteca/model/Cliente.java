18eed037f971d1a17685ae599ec3ceb76519d368
carol <dumont2700@gmail.com>
2024-05-31 14:07:39 -0300

    alterações

---
+++ src/main/java/com/example/biblioteca/model/Cliente.java
@@ -0,0 +1,48 @@
+package com.example.biblioteca.model;
+
+public class Cliente {
+    
+    private int id;
+    private String nome, cpf;
+
+    //Sobrecarga de construtores
+    public Cliente(){
+
+    }
+
+    public Cliente(String nome, String cpf){
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public Cliente(int id, String nome, String cpf){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getCpf(){
+        return cpf;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+}
